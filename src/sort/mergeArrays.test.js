be9752ef1644717080ffbdcfa11b8fb35c8d2031
anahi.gaetan <anahi.gaetan@bitlogic.io>
2022-02-27 23:10:15 -0300

    Arreglo la descripcion de los tests del merge

--- src/sort/mergeArrays.test.js
+++ src/sort/mergeArrays.test.js
@@ -8,13 +8,13 @@ describe('merge arrays function', () => {
         const result = mergeArrays(array1, array2);
         expect(result).toEqual(expected);
     })
-    test('should return first array if 2nd array is empty', () => {
+    test('should return first array if second array is empty', () => {
         const array1 = [1,2], array2 = [];
         const result = mergeArrays(array1, array2);
         expect(result).toEqual(array1);
     })
 
-    test('should return a second array if 1st array is empty', () => {
+    test('should return second array if first array is empty', () => {
         const array1 = [], array2 = [1,5,6];
         const result = mergeArrays(array1, array2);
         expect(result).toEqual(array2);
