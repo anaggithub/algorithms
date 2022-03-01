b1e99d50b623b337acd0a51b23383935eebdae3c
anahi.gaetan <anahi.gaetan@bitlogic.io>
2022-03-01 12:57:01 -0300

    Agrego version de fibonacci con closure

--- src/dynamic/dynamicFibonacci.js
+++ src/dynamic/dynamicFibonacci.js
@@ -1,18 +1,43 @@
-let cache = {};
+// Con el cache dentro, usando closure :D
 
 const dynamicFibonacci = (num) => {
-  if (cache[num]) {
-    console.log("entró al if cache: ", cache);
-    return cache[num];
-  } else {
-    if (num === 1) return 1;
-    if (num === 0) return 0;
-    const previous1 = dynamicFibonacci(num - 1);
-    const previous2 = dynamicFibonacci(num - 2);
-    cache[num] = previous1 + previous2;
-    console.log("en el else, el cache: ", cache);
-    return cache[num];
-  }
+  let cache = {};
+
+  const fibonacci = (num) => {
+    if (cache[num]) {
+      console.log("entró al if cache: ", cache);
+      return cache[num];
+    } else {
+      if (num === 1) return 1;
+      if (num === 0) return 0;
+      const previous1 = fibonacci(num - 1);
+      const previous2 = fibonacci(num - 2);
+      cache[num] = previous1 + previous2;
+      console.log("en el else, el cache: ", cache);
+      return cache[num];
+    }
+  };
+  console.log("el cache: ", cache);
+  return fibonacci(num);
 };
+
+// // Con el cache fuera
+// let cache = {};
+
+// const dynamicFibonacci = (num) => {
+
+//   if (cache[num]) {
+//     console.log("entró al if cache: ", cache);
+//     return cache[num];
+//   } else {
+//     if (num === 1) return 1;
+//     if (num === 0) return 0;
+//     const previous1 = dynamicFibonacci(num - 1);
+//     const previous2 = dynamicFibonacci(num - 2);
+//     cache[num] = previous1 + previous2;
+//     console.log("en el else, el cache: ", cache);
+//     return cache[num];
+//   }
+// };
 
 module.exports = dynamicFibonacci;
