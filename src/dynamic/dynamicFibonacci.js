a3c30919f2227381719a316332463b19e6f3a359
anahi.gaetan <anahi.gaetan@bitlogic.io>
2022-03-01 13:00:59 -0300

    Refactor en funcion fibonacci

--- src/dynamic/dynamicFibonacci.js
+++ src/dynamic/dynamicFibonacci.js
@@ -1,23 +1,21 @@
-// Con el cache dentro, usando closure :D
+// Con el cache dentro, usando closure
 
 const dynamicFibonacci = (num) => {
   let cache = {};
 
   const fibonacci = (num) => {
-    if (cache[num]) {
-      console.log("entró al if cache: ", cache);
-      return cache[num];
-    } else {
+    if (cache[num]) return cache[num];
+    else {
       if (num === 1) return 1;
       if (num === 0) return 0;
       const previous1 = fibonacci(num - 1);
       const previous2 = fibonacci(num - 2);
       cache[num] = previous1 + previous2;
-      console.log("en el else, el cache: ", cache);
+      console.log("el cache: ", cache);
       return cache[num];
     }
   };
-  console.log("el cache: ", cache);
+
   return fibonacci(num);
 };
 
